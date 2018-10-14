08a7f77c1f6ea50d4b2531c87ba96c7e3565475e
xavier <xaviersulkowski@gmail.com>
2018-10-14 22:59:18 +0200

    write dijkastra's algorithm and dijkastra's shortest path algorithm on my own

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -14,6 +14,43 @@ def read_graph_structure_scheme(graph_structure_scheme_file):
     return structure
 
 
+def dijkstra(graph, start):
+    S = set()
+    delta = dict.fromkeys(list(graph.adj), np.inf)
+    previous = dict.fromkeys(list(graph.adj), None)
+
+    delta[start] = 0
+
+    neighbors = {key: list(graph.neighbors(key)) for key in list(graph.node)}
+
+    while S != set(graph.node):
+        v = min((set(delta.keys())-S), key=delta.get)
+        for neighbor in set(neighbors[v]) - S:
+            new_path =delta[v] + graph[v][neighbor]['weight']
+            if new_path < delta[neighbor]:
+                delta[neighbor] = new_path
+                previous[neighbor] = v
+
+        S.add(v)
+
+    return (delta, previous)
+
+
+def shortest_path(graph, start, end):
+    delta, previous = dijkstra(graph, start)
+
+    path = []
+    vertex = end
+
+    while vertex is not None:
+        path.append(vertex)
+        vertex = previous[vertex]
+
+    path.reverse()
+
+    return path
+
+
 graph_structure_file = './dijkstra/graf.txt'
 structure_scheme = read_graph_structure_scheme(graph_structure_file)
 structure_scheme = np.asarray(structure_scheme, dtype=int)
@@ -21,10 +58,7 @@ structure_scheme = np.asarray(structure_scheme, dtype=int)
 graph = nx.DiGraph()
 graph.add_weighted_edges_from(structure_scheme)
 
-"""
-for line in structure_scheme:
-    graph.add_edge(line[0], line[1], weight=line[2])
-"""
+print(shortest_path(graph, 1, 20))
 
-print(nx.dijkstra_path(graph, 1, 20))
-# TODO: write above function on my own
+#   SHORT TEST
+assert nx.dijkstra_path(graph, 1, 20) == shortest_path(graph, 1, 20)
